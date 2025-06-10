dd2087302b035026bf3b488b992b1b85f1c05090
Alejandra Pabón Barbosa <alejandra_pabonb@soy.sena.edu.co>
2025-06-10 08:04:23 -0500

    Configurando los archivos App.jsx e index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import ElementoList from './components/ElementoList';
+import LibroList from './components/libroList';
+import LibroForm from './components/libroForm';
+import DVDList from './components/DVDList';
+import RevistaList from './components/RevistaList';
+import DVDForm from "./components/DVDForm.jsx";
+import RevistaForm from "./components/RevistaForm.jsx";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+    return (
+        <>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<ElementoList />} />
+                <Route path="/libros" element={<LibroList />} />
+                <Route path="libros/nuevo" element={<LibroForm />} />
+                <Route path="libros/editar/:id" element={<LibroForm />} />
+                <Route path="/dvds" element={<DVDList />} />
+                <Route path="/dvds/nuevo" element={<DVDForm />} />
+                <Route path="/dvds/editar/:id" element={<DVDForm />} />
+                <Route path="/revistas" element={<RevistaList />} />
+                <Route path="/revistas/nuevo" element={<RevistaForm />} />
+                <Route path="/revistas/editar/:id" element={<RevistaForm />} />
+            </Routes>
+        </>
+    );
+};
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+export default App;
